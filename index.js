3497f79f45671ed4d901af0a0ceaf138de3ec4ad
Diego Lao Tebar <howarto@ymail.com>
2018-04-08 23:19:41 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const { Composer, log } = require("micro-bot");
+
+const bot = new Composer();
+
+bot.use(log());
+bot.start(({ reply }) => reply("Hey there!"));
+bot.command("help", ({ reply }) => reply("Help message"));
+bot.command("about", ({ reply }) => reply("About message"));
+bot.command("/smbc", ctx => {
+  reply("Smbc command");
+  getRandomPic(ctx);
+});
+
+// Auxiliary functions
+function getRandomPic(ctx) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var domParser = new DOMParser();
+      var smbcHtml = domParser.parseFromString(xhttp.responseText, "text/html");
+      var comicImg = smbcHtml.getElementById("cc-comic");
+      var comicImgUrl =
+        "https://www.smbc-comics.com" + comicImg.getAttribute("src");
+      ctx.replyWithPhoto({ url: comicImgUrl });
+    }
+  };
+  xhttp.open("GET", "https://www.smbc-comics.com/random.php", true);
+  xhttp.send();
+}
+
+function goToImgLinkToDownload(url) {
+  var linkElement = document.createElement("a");
+  linkElement.href = url;
+  linkElement.download = "img.png";
+  document.body.append(linkElement);
+  linkElement.click();
+  linkElement.remove();
+}
+
+// Export bot
+module.exports = bot;
---
+++ readme.md
@@ -0,0 +1,30 @@
+# webcomicbutlerbot
+
+[![Build Status](https://travis-ci.org/howarto/webcomicbutlerbot.svg?branch=master)](https://travis-ci.org/howarto/webcomicbutlerbot)
+
+[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/howarto/webcomicbutlerbot)
+
+## Usage
+
+```sh
+$ npm install
+$ BOT_TOKEN='123:......' npm run dev
+```
+
+```sh
+$ yarn
+$ BOT_TOKEN='123:......' yarn dev
+```
+
+## Deployment
+
+This bot can be deployed to [now](https://zeit.co/now) by Zeit.
+Assuming you've got `now` installed and set up:
+
+```sh
+$ now -e BOT_TOKEN='123:......' howarto/webcomicbutlerbot
+```
+
+Alternative, deploy right now without even leaving the browser:
+
+[![Deploy to now](https://deploy.now.sh/static/button.svg)](https://deploy.now.sh/?repo=https://github.com/howarto/webcomicbutlerbot)
