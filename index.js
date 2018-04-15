ffea6a5a478b10be6ed5d9de865538e4643d81cd
Diego Lao Tebar <howarto@ymail.com>
2018-04-15 23:01:37 +0200

    Added support for darklegacy comics

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ bot.start(({ reply }) =>
 );
 bot.command("about", ({ reply }) => reply("Author: Diego Lao <howarto>"));
 bot.command("/smbc", ctx => {
-  getRandomPic(ctx);
+  getRandomPicSMBC(ctx);
+});
+bot.command("/dl", ctx => {
+  getRandomPicDarkLegacy(ctx);
 });
 
-// Auxiliary functions
-function getRandomPic(ctx) {
+// Command functions
+
+function getRandomPicSMBC(ctx) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -25,7 +29,7 @@ function getRandomPic(ctx) {
       var smbcHtml = new JSDOM(xhttp.responseText);
       var comicImg = smbcHtml.window.document.getElementById("cc-comic");
       var comicImgUrl =
-        "https://www.smbc-comics.com" + comicImg.getAttribute("src");
+      "https://www.smbc-comics.com" + comicImg.getAttribute("src");
       ctx.replyWithPhoto({ url: comicImgUrl });
     }
   };
@@ -39,5 +43,33 @@ function getRandomPic(ctx) {
   xhttp.send();
 }
 
+function getRandomPicDarkLegacy(ctx) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var html = xhttp.responseText;
+      var stringToSearch = "var iLatestComic = ";
+      var startIndex = html.indexOf(stringToSearch);
+      var endIndex = html.indexOf(";", startIndex + stringToSearch.length);
+      var lastImageNumber = html.substring(startIndex + stringToSearch.length, endIndex);
+      // Generates an url with the values between 1 and the last comic uploaded
+      var imgUrl = "http://www.darklegacycomics.com/comics/" + getRandomInt(1, lastImageNumber) + ".jpg";
+      ctx.replyWithPhoto({ url: imgUrl });
+    }
+  };
+  xhttp.open(
+    "GET",
+    "https://cors-anywhere.herokuapp.com/http://www.darklegacycomics.com/1",
+    true
+  );
+  xhttp.setRequestHeader("origin", "*");
+  xhttp.setRequestHeader("x-requested-with", "*");
+  xhttp.send();
+}
+
+// Auxiliary functions
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 // Export bot
 module.exports = bot;
