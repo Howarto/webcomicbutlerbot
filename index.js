a8a464ff160d80e8f8a00c092df93558a5bc1b09
Diego Lao Tebar <howarto@ymail.com>
2018-04-15 21:17:05 +0200

    fix

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const DEBUG = true;
 const { Composer, log } = require("micro-bot");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const jsdom = require("jsdom");
@@ -9,7 +8,6 @@ bot.use(log());
 bot.start(({ reply }) => reply("Hey there!"));
 bot.command("help", ({ reply }) => reply("Help message"));
 bot.command("about", ({ reply }) => reply("About message"));
-bot.command("/img1", (ctx) => ctx.replyWithPhoto({ url: "https://spectratherapies.com/wp-content/uploads/2017/06/LSS-Autism-Acceptance.jpg" }));
 bot.command("/smbc", ctx => {
   getRandomPic(ctx);
 });
@@ -18,9 +16,7 @@ bot.command("/smbc", ctx => {
 function getRandomPic(ctx) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
-    if (DEBUG) ctx.reply(this.readyState + " " + this.status + " " + this.statusText);
     if (this.readyState == 4 && this.status == 200) {
-      debugger;
       var smbcHtml = new JSDOM(xhttp.responseText);
       var comicImg = smbcHtml.window.document.getElementById("cc-comic");
       var comicImgUrl =
@@ -34,16 +30,6 @@ function getRandomPic(ctx) {
   xhttp.setRequestHeader('origin', '*');
   xhttp.setRequestHeader('x-requested-with', '*');
   xhttp.send();
-  if (DEBUG) ctx.reply("END");
-}
-
-function goToImgLinkToDownload(url) {
-  var linkElement = document.createElement("a");
-  linkElement.href = url;
-  linkElement.download = "img.png";
-  document.body.append(linkElement);
-  linkElement.click();
-  linkElement.remove();
 }
 
 // Export bot
