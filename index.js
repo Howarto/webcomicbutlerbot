de8b2e902b7f1a8c6b83645cc91572384d26cb1e
Diego Lao Tebar <howarto@ymail.com>
2018-04-15 21:34:13 +0200

    fix

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const bot = new Composer();
 
-bot.webhookReply = false;
 bot.use(log());
-bot.start(({ reply }) => reply("Hey there!"));
-bot.command("help", ({ reply }) => reply("Help message"));
-bot.command("about", ({ reply }) => reply("About message"));
+bot.start(({ reply }) =>
+  reply(
+    "Hey there, I'm WebComicButlerBot. I can send you webcomics from different websites"
+  )
+);
+bot.command("about", ({ reply }) => reply("Author: Diego Lao <howarto>"));
 bot.command("/smbc", ctx => {
   getRandomPic(ctx);
 });
@@ -18,19 +20,22 @@ function getRandomPic(ctx) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        // ctx.reply("algo");
+      //   It avoids write bug on chatbot framework. It does not write anything visible.
+      ctx.reply("Something");
       var smbcHtml = new JSDOM(xhttp.responseText);
       var comicImg = smbcHtml.window.document.getElementById("cc-comic");
       var comicImgUrl =
-      "https://www.smbc-comics.com" + comicImg.getAttribute("src");
+        "https://www.smbc-comics.com" + comicImg.getAttribute("src");
       ctx.replyWithPhoto({ url: comicImgUrl });
     }
   };
-  xhttp.onerror = function() {
-  };
-  xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://www.smbc-comics.com/random.php", true);
-  xhttp.setRequestHeader('origin', '*');
-  xhttp.setRequestHeader('x-requested-with', '*');
+  xhttp.open(
+    "GET",
+    "https://cors-anywhere.herokuapp.com/https://www.smbc-comics.com/random.php",
+    true
+  );
+  xhttp.setRequestHeader("origin", "*");
+  xhttp.setRequestHeader("x-requested-with", "*");
   xhttp.send();
 }
 
